ef14c168005b3af59a18c8c0c1179c57550c67f4
mac2UZaM583 <163586801+mac2UZaM583@users.noreply.github.com>
2024-06-04 13:17:28 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 from name import get_balance, get_tickers, place_order
+from name_test_1 import place_order as place_order_test_1
 import asyncio
 
 headers = {'User-Agent': 'Opera/9.80 (Windows NT 6.2; WOW64) Presto/2.12.388 Version/12.17'}
-url_count = 746
+url_count = 753
 
 # Информация для торговли
-tp = 0.012
+tp = 0.024
 sl = 0.012
 
 # Отсчет времени и количества запросов
@@ -46,10 +47,12 @@ async def main():
                             if str(content).count('🔴') == 1:
                                 if ticker in tickers and balance_usdt != 0:
                                     await place_order(ticker, 1, qty, tp, sl)
+                                    await place_order_test_1(ticker, 1, qty*2, 0.012, sl)
 
                             if str(content).count('🟢') == 1:
                                 if ticker in tickers and balance_usdt != 0:
                                     await place_order(ticker, 0, qty, tp, sl)
+                                    await place_order_test_1(ticker, 0, qty*2, 0.012, sl)
                             
                             break
                     i += 1
